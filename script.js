57a3750e66d16fcfcc66d4bf0411c88c25718c58
electric663hair <electric663hair@gmail.com>
2024-09-05 10:44:14 +0200

    Made bad login system8not a login system) just fun

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function login() {
-    alert("Login Detected!")
+    username = window.prompt("Type in your username:");
+    password = window.prompt("Whats your password:");
+    var logedIn;
+
+    if (username == "SkilledApik" && password == "Mark") {
+        alert("Login Accessed!")
+        logedIn = true;
+    } else {
+        alert("Access Denied!")
+    }
+
+    if (logedIn) {
+        loginButton = document.getElementById("loginButton");
+    }
 }
